e05e19f6e5313514a96b7ee95a37c2867146ff65
martintierro <martintierro15@gmail.com>
2019-10-15 17:24:44 +0800

    fixed sql file

--- sql/testdb_renamed.sql
+++ sql/testdb_renamed.sql
@@ -26,8 +26,8 @@ DROP TABLE IF EXISTS `admin`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `admin` (
   `admin_id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `username` varchar(999) NOT NULL,
+  `password` varchar(999) NOT NULL,
   PRIMARY KEY (`admin_id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
